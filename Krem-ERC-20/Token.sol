3f7d1625490e0814a481c532fa2267edafea0561
David Kremenitzer <dkrem21@gmail.com>
2021-11-01 14:48:20 -0400

    updated contract with approve func

--- Krem-ERC-20/Token.sol
+++ Krem-ERC-20/Token.sol
@@ -8,11 +8,11 @@ contract Token {
     uint256 public decimals;
     uint256 public totalSupply;
 
-    // Keeps track of my balances and allowances approved
+
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
-    // Events - fire events on state changes etc
+
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
@@ -24,7 +24,6 @@ contract Token {
         balanceOf[msg.sender] = totalSupply;
     }
 
-
     function transfer(address _to, uint256 _value) external returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
         _transfer(msg.sender, _to, _value);
@@ -33,16 +32,11 @@ contract Token {
 
 
     function _transfer(address _from, address _to, uint256 _value) internal {
-        // Ensure sending is to valid address! 0x0 address cane be used to burn()
         require(_to != address(0));
         balanceOf[_from] = balanceOf[_from] - (_value);
         balanceOf[_to] = balanceOf[_to] + (_value);
         emit Transfer(_from, _to, _value);
     }
-
-    ///  Approve function allowes _spender (uniswap or coinbase ect ) to spend on my behalf
-    // Part1 - Function approve gets my permission
-    // Part 2 - function transferFrom then transfers my coin to whoever is buying
 
     function approve(address _spender, uint256 _value) external returns (bool) {
         require(_spender != address(0));
